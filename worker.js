7a6e7ad92f683f1158175213e191f735c101a835
Mohitsingh <142308041+mohit-100@users.noreply.github.com>
2026-03-05 11:43:00 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,8 +1,25 @@
 const { parentPort } = require("worker_threads");
 
 parentPort.on("message", (task) => {
-    // Do heavy work here
-    const result = task * 2;  // example work
+    try {
 
-    parentPort.postMessage(result);
+        const fn = eval(`(${task.fn})`);
+
+        const result = fn(...task.args);
+
+        parentPort.postMessage({
+            id: task.id,
+            result: result,
+            error: null
+        });
+
+    } catch (error) {
+
+        parentPort.postMessage({
+            id: task.id,
+            result: null,
+            error: error.message
+        });
+
+    }
 });
